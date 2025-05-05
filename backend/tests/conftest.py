043180d762f28e7394ea5e164043a893cb4162df
Sergey Gendel <sergendel@gmail.com>
2025-05-05 16:18:34 -0400

    working irectory for gitactions set to fitbeat and not backend

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,4 +2,4 @@ import sys
 from pathlib import Path
 
 # explicitly add the root directory to the Python path
-sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
